0c1fdb072c5c86120d2c86e4c7b2db93b8e75887
Karlos <karlos.hbraga@gmail.com>
2020-01-22 21:31:12 -0300

    Validações e permissões usando o NestJS.

---
+++ 4tech-backend2020/src/domain/login.viewmodel.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty, MaxLength, Max, Length } from 'class-validator';
+
+export class LoginViewModel{
+    @IsNotEmpty()
+    @Length(3, 10)
+    readonly userLogin: string;
+    @IsNotEmpty()
+    @Length(3, 10)
+    readonly userPassword: string;
+}
---
+++ 4tech-backend2020/src/domain/user.viewmodel.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, MaxLength, Max, Length } from 'class-validator';
+
+export class UserViewModel{
+    @IsNotEmpty()
+    @Length(3, 10)
+    readonly userLogin: string;
+    @IsNotEmpty()
+    @Length(3, 10)
+    readonly userName: string;
+    @IsNotEmpty()
+    @Length(3, 10)
+    readonly password: string;
+}
