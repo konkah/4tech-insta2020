031ea4acf6cd9a713b60f7c555844e9942268480
Karlos <karlos.hbraga@gmail.com>
2020-01-26 21:35:28 -0300

    Correção do React no Login.

--- 4tech-frontend2020/src/containers/Login/Login.js
+++ 4tech-frontend2020/src/containers/Login/Login.js
@@ -1,7 +1,7 @@
-import React, { userState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Login = ({ name, age }) => {
-    const [login, setLogin] = userState('');
+    const [login, setLogin] = useState('');
 
     useEffect(() => {
         console.log('Alterei o Login');
